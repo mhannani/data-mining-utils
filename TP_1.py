bd5dcc6e6fbbdd04ec2e95a49df5c56497ec703a
mhannani <mohamed.hanani2019@gmail.com>
2021-04-17 00:55:11 +0000

    Manhattan distance

--- TP_1.py
+++ TP_1.py
@@ -2,8 +2,10 @@
 import numpy as np
 from distance.euclidean import euclidean
 from distance.minkowski import minkowski
+from distance.manhattan import manhattan
 A1 = np.array([[1, 4, 3, 4, 5], [1, 5, 3, 4, 5]])
 A2 = np.array([[1, 2, 3, 4, 5], [1, 2, 3, 4, 5]])
 
 # print(euclidean(A1, A2))
-print(minkowski(A1, A2, 5))
+# print(minkowski(A1, A2, 5))
+print(manhattan(A1, A2))
---
+++ distance/manhattan.py
@@ -0,0 +1,21 @@
+import numpy as np
+
+
+def manhattan(u, v):
+    """
+    Calculates the manhattan distance.
+    :param u: ndarray-like
+    :param v: ndarray-like
+    :return: the manhattan distance
+    """
+    dist = 0
+    if not isinstance(u, np.ndarray) or not isinstance(v, np.ndarray):
+        raise ValueError('Please provide two ndarray !')
+    else:
+        flat_u = u.flatten()
+        flat_v = v.flatten()
+
+    for u_i, v_i in zip(flat_u, flat_v):
+        dist += sum([abs(u_i - v_i)])
+
+    return dist
