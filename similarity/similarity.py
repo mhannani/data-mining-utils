aa687aa6689a68021a76cfc400048b3d53f5b619
HANNANI Mohamed <49957450+mhannani@users.noreply.github.com>
2021-05-02 18:11:59 +0000

    tweaking to the wheel.

--- similarity/similarity.py
+++ similarity/similarity.py
@@ -79,27 +79,3 @@ class Measures:
         cosine_similarity = np.dot(u, v) / (np.linalg.norm(u) * np.linalg.norm(v))
 
         return cosine_similarity
-
-    @staticmethod
-    def minkowski(u, v, p):
-        """
-        Calculates the minkowski distance between two vectors.
-        :param u: ndarray-like
-        :param v: ndarray-like
-        :param p: scalar
-        :return: The minkowski distance
-        """
-        dist = 0
-        if p < 1:
-            raise ValueError('p must be at least 1')
-
-        # if the the dimensional representation of the image is an ndarray
-        if not isinstance(u, np.ndarray) or not isinstance(v, np.ndarray):
-            raise ValueError('Please provide two ndarray !')
-
-        else:
-            flat_u = u.flatten()
-            flat_v = v.flatten()
-
-        dist += pow((sum([pow((int(u_i) - int(v_i)), p) for u_i, v_i in zip(flat_u, flat_v)])), 1 / p)
-        return dist
