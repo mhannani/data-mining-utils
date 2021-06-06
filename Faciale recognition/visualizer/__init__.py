cd2d683daaaa3a19249f12bd24b2a92137ac1696
HANNANI Mohamed <49957450+mhannani@users.noreply.github.com>
2021-06-06 23:13:03 +0100

    pca method

--- Faciale recognition/visualizer/__init__.py
+++ Faciale recognition/visualizer/__init__.py
@@ -1,5 +1,7 @@
 from .show_some_images import show_some_images
+from .plot_pca_vs_variance import pca_vs_variance
 
 __all__ = [
     'show_some_images',
+    'pca_vs_variance'
 ]
---
+++ Faciale recognition/visualizer/plot_pca_vs_variance.py
@@ -0,0 +1,22 @@
+import matplotlib.pyplot as plt
+from sklearn.decomposition import PCA
+import numpy as np
+
+
+def pca_vs_variance(imgs, variance=0.95):
+    """
+    Plot the pca versus variance to choose accurately the number of components.
+    :param variance: double
+        The variance.
+    :param imgs: array_like
+        Array of images..
+    """
+
+    # Fit the model
+    pca = PCA().fit(imgs)
+    y = np.cumsum(pca.explained_variance_ratio_)
+    plt.axhline(y=variance, color='r', linestyle='-')
+    plt.text(0.1, variance + 0.02, f'{variance * 100}% cut-off threshold', color='red', fontsize=14)
+    plt.plot(y, linestyle='--', color='b')
+    plt.xlabel('number of components')
+    plt.ylabel('cumulative explained variance')
